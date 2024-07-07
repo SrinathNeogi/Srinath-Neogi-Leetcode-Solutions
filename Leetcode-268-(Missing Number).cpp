7d8cc4e8735f7c5cd7432ba2b880a00fc0f5c460
SrinathNeogi <srinathneogicse@gmail.com>
2024-07-08 01:42:00 +0530

    Update Leetcode-268-(Missing Number).cpp
    C++

--- Leetcode-268-(Missing Number).cpp
+++ Leetcode-268-(Missing Number).cpp
@@ -15,3 +15,32 @@ public:
         return missing;
     }
 };
+//Method 2
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int missing = 0;
+
+        if(nums[0] != 0)
+        {
+            missing = 0;
+        }
+        else if(nums[n-1] != n)
+        {
+            missing = n;
+        }
+        else
+        {
+            for(int i=0; i<n-1; i++)
+            {
+                if(nums[i+1] - nums[i] != 1)
+                {
+                    missing = (nums[i+1] - 1);
+                }
+            }
+        }
+        return missing;
+    }
+};
